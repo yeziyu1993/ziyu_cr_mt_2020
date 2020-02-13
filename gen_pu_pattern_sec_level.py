22ef543a65f50a7ac3fdf5f6c16479ad972cfc58
yeziyu1993 <40275696+yeziyu1993@users.noreply.github.com>
2020-02-13 12:01:12 -0800

    Create gen_pu_pattern_sec_level.py

---
+++ gen_pu_pattern_sec_level.py
@@ -0,0 +1,7 @@
+###############################################################################
+# This file implements the algorithm for generating PU on-off pattern on the timescale of seconds
+# proposed in
+#   Modeling and Simulation of Time-Correlation Properties of Spectrum Use in Cognitive Radio
+#   by Miguel López-Benítez and Fernando Casadevall
+#   Available at: https://www.researchgate.net/publication/224259687_Modeling_and_Simulation_of_Time-Correlation_Properties_of_Spectrum_Use_in_Cognitive_Radio
+###############################################################################
